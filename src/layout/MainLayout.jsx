66d463734dd8e440e28c83955c2b7c01da95a011
ajay-prasad-052 <ajaykushwaha4312@gmail.com>
2026-09-07 14:12:03 +0530

    fix the deployment

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import WhatsAppButton from "../components/WhatsappButton";
+import WhatsAppButton from "../components/Whatsappbutton";
 
 function MainLayout() {
   return (
