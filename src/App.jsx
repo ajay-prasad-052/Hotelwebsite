1a31f497e735c99772a877339d6a0e52910e1f8b
ajay-prasad-052 <ajaykushwaha4312@gmail.com>
2026-09-07 13:56:35 +0530

    fix hotel website deploymen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import MainLayout from "./layouts/MainLayout";
+import MainLayout from "./layout/MainLayout";
 
 import Home from "./pages/Home";
 import Rooms from "./pages/Room";
