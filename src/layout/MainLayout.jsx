27a073c4d915f1dd51a7afdffade2905c22e4e2b
ajay-prasad-052 <ajaykushwaha4312@gmail.com>
2026-09-07 13:45:03 +0530

    fix hotel website deployment

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import WhatsAppButton from "../components/WhatsAppButton";
+import WhatsAppButton from "../components/WhatsappButton";
 
 function MainLayout() {
   return (
